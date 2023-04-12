d76bc496e94c942afdb65c145601fa0e598069af
raduubraduu <lefter_radu@yahoo.com>
2023-04-12 22:04:39 +0100

    implemented cathcing errors with errorboundary

--- app/root.jsx
+++ app/root.jsx
@@ -1,10 +1,12 @@
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
 } from "@remix-run/react";
 
 import styles from "~/styles/main.css";
@@ -29,6 +31,35 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  return (
+    <html lang="en">
+        <head>
+          <Meta />
+          <Links />
+          <title>An error occurred!</title>
+        </head>
+        <body>
+          <header>
+            <MainNavigation />
+          </header>
+          <main className="error">
+            <h1>An error occurred!</h1>
+            <p>{error.message}</p>
+            <p>
+              Back to <Link to="/">safety</Link>!
+            </p>
+          </main>
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </body>
+      </html>
   );
 }
 
--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,5 +1,5 @@
 import { redirect } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { Link, useLoaderData, useRouteError } from '@remix-run/react';
 
 import NewNote, { links as newNoteLinks } from '~/components/NewNote';
 import NoteList, { links as noteListLinks } from '~/components/NoteList';
@@ -34,6 +34,19 @@ export default function NotesPage() {
     const updatedNotes = existingNotes.concat(noteData);
     await storeNotes(updatedNotes);
     return redirect('/notes');
+  }
+
+  export function ErrorBoundary() {
+    const error = useRouteError();
+    return (
+      <main className="error">
+        <h1>An error related to your notes occurred!</h1>
+        <p>{error.message}</p>
+        <p>
+          Back to <Link to="/">safety</Link>!
+        </p>
+      </main>
+    );
   }
 
   export function links() {
