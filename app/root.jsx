0e71808214e9af7deed08eeea6264bdf235411c7
raduubraduu <lefter_radu@yahoo.com>
2023-04-12 22:28:17 +0100

    implemented meta function

--- app/root.jsx
+++ app/root.jsx
@@ -12,6 +12,12 @@ import {
 import styles from "~/styles/main.css";
 import MainNavigation from "~/components/MainNavigation";
 
+export const meta = () => [
+  { charset: "utf-8" },
+  { title: "New Remix App" },
+  { viewport: "width=device-width,initial-scale=1" },
+];
+
 export default function App() {
   return (
     <html lang="en">
@@ -39,27 +45,27 @@ export function ErrorBoundary() {
 
   return (
     <html lang="en">
-        <head>
-          <Meta />
-          <Links />
-          <title>An error occurred!</title>
-        </head>
-        <body>
-          <header>
-            <MainNavigation />
-          </header>
-          <main className="error">
-            <h1>An error occurred!</h1>
-            <p>{error.message}</p>
-            <p>
-              Back to <Link to="/">safety</Link>!
-            </p>
-          </main>
-          <ScrollRestoration />
-          <Scripts />
-          <LiveReload />
-        </body>
-      </html>
+      <head>
+        <Meta />
+        <Links />
+        <title>An error occurred!</title>
+      </head>
+      <body>
+        <header>
+          <MainNavigation />
+        </header>
+        <main className="error">
+          <h1>An error occurred!</h1>
+          <p>{error.message}</p>
+          <p>
+            Back to <Link to="/">safety</Link>!
+          </p>
+        </main>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
   );
 }
 
--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,54 +1,61 @@
-import { redirect } from '@remix-run/node';
-import { Link, useLoaderData, useRouteError } from '@remix-run/react';
+import { redirect } from "@remix-run/node";
+import { Link, useLoaderData, useRouteError } from "@remix-run/react";
 
-import NewNote, { links as newNoteLinks } from '~/components/NewNote';
-import NoteList, { links as noteListLinks } from '~/components/NoteList';
-import { getStoredNotes, storeNotes } from '~/data/notes';
+import NewNote, { links as newNoteLinks } from "~/components/NewNote";
+import NoteList, { links as noteListLinks } from "~/components/NoteList";
+import { getStoredNotes, storeNotes } from "~/data/notes";
 
 export default function NotesPage() {
   const notes = useLoaderData();
-  
-    return (
-      <main>
-        <NewNote />
-        <NoteList notes={notes} />
-      </main>
-    );
+
+  return (
+    <main>
+      <NewNote />
+      <NoteList notes={notes} />
+    </main>
+  );
+}
+
+export async function loader() {
+  const notes = await getStoredNotes();
+  return notes;
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const noteData = Object.fromEntries(formData);
+
+  if (noteData.title.trim().length < 5) {
+    return { message: "Invalid title - must be at least 5 characters long." };
   }
 
-  export async function loader() {
-    const notes = await getStoredNotes();
-    return notes;
-  }
+  const existingNotes = await getStoredNotes();
+  noteData.id = new Date().toISOString();
+  const updatedNotes = existingNotes.concat(noteData);
+  await storeNotes(updatedNotes);
+  return redirect("/notes");
+}
 
-  export async function action({ request }) {
-    const formData = await request.formData();
-    const noteData = Object.fromEntries(formData);
+export function meta() {
+  return [
+    { title: "All Notes" },
+    { description: "Manage your notes with ease." },
+  ];
+}
 
-    if (noteData.title.trim().length < 5) {
-      return { message: 'Invalid title - must be at least 5 characters long.' };
-    }
+export function ErrorBoundary() {
+  const error = useRouteError();
+  return (
+    <main className="error">
+      <h1>An error related to your notes occurred!</h1>
+      <p>{error.message}</p>
+      <p>
+        Back to <Link to="/">safety</Link>!
+      </p>
+    </main>
+  );
+}
 
-    const existingNotes = await getStoredNotes();
-    noteData.id = new Date().toISOString();
-    const updatedNotes = existingNotes.concat(noteData);
-    await storeNotes(updatedNotes);
-    return redirect('/notes');
-  }
-
-  export function ErrorBoundary() {
-    const error = useRouteError();
-    return (
-      <main className="error">
-        <h1>An error related to your notes occurred!</h1>
-        <p>{error.message}</p>
-        <p>
-          Back to <Link to="/">safety</Link>!
-        </p>
-      </main>
-    );
-  }
-
-  export function links() {
-    return [...newNoteLinks(), ...noteListLinks()];
-  }
+export function links() {
+  return [...newNoteLinks(), ...noteListLinks()];
+}
